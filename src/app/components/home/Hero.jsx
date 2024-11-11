66832645e306f58865bf14f3bde8fcdd7d8d4b34
oyewosolomon <oyewosolomon@gmail.com>
2024-11-11 10:59:03 +0100

    New update

--- src/app/components/home/Hero.jsx
+++ src/app/components/home/Hero.jsx
@@ -4,7 +4,7 @@ const hero = () => {
   return (
     <div className='h-screen w-full relative'>
         <div className='absolute place-items-center flex text-center justify-center inset-0 bg-[url("/assets/images/image_2.png")] bg-cover bg-center bg-no-repeat'>
-Hello
+
         </div>
 
     </div>
